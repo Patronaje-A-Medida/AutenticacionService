695f249aa9cfb9ef1c0bf923be9bebafeb27bbd7
Jeiel <jeiel.disas@gmail.com>
2021-12-04 00:40:59 -0500

    #10 creacion DTO para registro de usuario de rol technician y refactorizacion de clases DTO

--- AutenticacionService.Domain/Models/UserTechnicianCreate.cs
+++ AutenticacionService.Domain/Models/UserTechnicianCreate.cs
@@ -5,13 +5,17 @@ using System.Text;
 
 namespace AutenticacionService.Domain.Models
 {
-    public class UserOwnerCreate : UserBaseCreate
+    public class UserTechnicianCreate : UserBaseCreate
     {
         [Required(ErrorMessage = "El DNI no puede ser nulo")]
         [MaxLength(8, ErrorMessage = "El DNI debe contener 8 caracteres")]
         [MinLength(8, ErrorMessage = "El DNI debe contener 8 caracteres")]
         public string Dni { get; set; }
 
-        public AtelierCreate Atelier { get; set; }
+        [Required(ErrorMessage = "El Id del Jefe no puede ser nulo")]
+        public int BossId { get; set; }
+
+        [Required(ErrorMessage = "El Id del Atelier no puede ser nulo")]
+        public int AtelierId { get; set; }
     }
 }
