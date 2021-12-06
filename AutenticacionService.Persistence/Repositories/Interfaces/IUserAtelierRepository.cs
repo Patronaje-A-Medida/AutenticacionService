d8633fd02b3ac3e4da8c15bdc4339e22a4e890e0
Jeiel <jeiel.disas@gmail.com>
2021-12-05 23:18:40 -0500

    #12 agregado funcion get by user id and role a repository user atelier

--- AutenticacionService.Persistence/Repositories/Interfaces/IUserAtelierRepository.cs
+++ AutenticacionService.Persistence/Repositories/Interfaces/IUserAtelierRepository.cs
@@ -1,8 +1,10 @@
 ﻿using AutenticacionService.Domain.Entities;
+using System.Threading.Tasks;
 
 namespace AutenticacionService.Persistence.Repositories.Interfaces
 {
     public interface IUserAtelierRepository : IRepository<UserAtelier>
     {
+        Task<UserAtelier> GetByUserId_Role(string userId, string role);
     }
 }
